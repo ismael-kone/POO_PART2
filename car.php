5fb730ceb114bcd960dbd91967d2c97aea7919f1
Ismael Kone <89470487+ismael-kone@users.noreply.github.com>
2021-10-25 11:21:36 +0200

    car version

---
+++ car.php
@@ -0,0 +1,50 @@
+
+<?php
+require_once 'Vehicle.php';
+
+class Car extends Vehicle
+{
+    
+    private string $energy;
+
+    private int $energyLevel;
+
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+    
+    public function __construct(string $color, int $nbSeats, string $energy){
+        parent::__construct($color, $nbSeats);
+        $this->energy = $energy;
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+/**
+     * Set the value of energy
+     *
+     * @return  self
+     */ 
+    public function setEnergy(string $energy): Car
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+
+    
+}
